a5c0332bc0134a81a21abab8b461d95ae27e19b8
Mezz <mezzanine510@gmail.com>
2024-12-22 14:48:22 -0800

    base active player state and functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react";
 import Player from './components/Player';
 import GameBoard from "./components/GameBoard.jsx";
 
 function App() {
+    const [activePlayer, setActivePlayer] = useState('X');
+
+    function handleSelectCell() {
+        setActivePlayer((currentPlayer) => currentPlayer === 'X' ? 'O' : 'X');
+    }
+    console.log(activePlayer);
+
     return (
         <main>
             <div id="game-container">
-                <ol id="players">
-                    <Player initialName="Player 1" symbol="X" />
-                    <Player initialName="Player 2" symbol="O" />
+                <ol id="players" className="highlight-player">
+                    <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
+                    <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
                 </ol>
-                <GameBoard />
+                <GameBoard onClickCell={handleSelectCell} activePlayerSymbol={activePlayer}/>
             </div>
         </main>
-    )
+    );
 }
 
 export default App
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,15 +6,17 @@ const initialGameBoard = [
     [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onClickCell, activePlayerSymbol }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleClickCell(rowIndex, colIndex) {
         setGameBoard((previousGameBoard) => {
             const updatedGameBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
-            updatedGameBoard[rowIndex][colIndex] = 'X';
+            updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedGameBoard;
         });
+
+        onClickCell();
     }
 
     return (
