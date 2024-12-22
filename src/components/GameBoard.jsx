eecd97d8c514a7e765ef66f99f942e16653134c8
Mezz <mezzanine510@gmail.com>
2024-12-22 14:26:21 -0800

    gameboard now updates when cells are clicked

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,14 +7,24 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleClickCell(rowIndex, colIndex) {
+        setGameBoard((previousGameBoard) => {
+            const updatedGameBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
+            updatedGameBoard[rowIndex][colIndex] = 'X';
+            return updatedGameBoard;
+        });
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) =>
+            {gameBoard.map((row, rowIndex) =>
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) =>
                             <li key={colIndex}>
-                                <button>{playerSymbol}</button>
+                                <button onClick={()=>handleClickCell(rowIndex, colIndex)}>{playerSymbol}</button>
                             </li>
                         )}
                     </ol>
