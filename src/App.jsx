605ac1050cc43cc824efb5a1ef18cc9369b80cba
Mezz <mezzanine510@gmail.com>
2024-12-22 18:39:12 -0800

    code cleanup for readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,56 @@
-import { useState } from "react";
+import {useState} from "react";
 import Player from './components/Player';
 import GameBoard from "./components/GameBoard.jsx";
 import Log from "./components/Log.jsx";
 import GameOver from "./components/GameOver.jsx";
-import { WINNING_COMBINATIONS} from "./winning-combinations.js";
+import {WINNING_COMBINATIONS} from "./winning-combinations.js";
+import {INITIAL_GAME_BOARD, PLAYERS} from "./GAME_CONSTANTS.js";
 
 
-const initialGameBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
-
-
-function App() {
-    const [players, setPlayers] = useState({
-        X: 'Player 1',
-        O: 'Player 2'
-    });
-    const [gameTurns, setGameTurns] = useState([]);
-    const [activePlayer, setActivePlayer] = useState('X');
-
-    // Deep copy the initial game board
-    let gameBoard = [...initialGameBoard.map(row => [...row])];
+// Deep copy the initial game board
+function deriveGameBoard(gameTurns) {
+    let gameBoard = [...INITIAL_GAME_BOARD.map(row => [...row])];
 
     for (const turn of gameTurns) {
-        const { cell, player } = turn;
-        const { row, col } = cell;
+        const {cell, player} = turn;
+        const {row, col} = cell;
 
         gameBoard[row][col] = player;
     }
 
+    return gameBoard;
+}
+
+function deriveWinner(gameBoard, players) {
     let winner = null;
     for (const combination of WINNING_COMBINATIONS) {
         const symbols = combination.map(
-            ({ row, column }) => gameBoard[row][column]
+            ({row, column}) => gameBoard[row][column]
         );
 
         if (symbols.every((symbol) => symbol && symbol === symbols[0])) {
             winner = players[symbols[0]];
-            console.log(players);
         }
     }
 
+    return winner;
+}
+
+
+function App() {
+    const [players, setPlayers] = useState(PLAYERS);
+    const [gameTurns, setGameTurns] = useState([]);
+    const [activePlayer, setActivePlayer] = useState('X');
+
+    const gameBoard = deriveGameBoard(gameTurns);
+    const winner = deriveWinner(gameBoard, players);
     const hasDraw = gameTurns.length === 9 && !winner;
 
     function handleClickCell(rowIndex, colIndex) {
         setGameTurns(prevTurns => {
             const updatedTurns = [
                 {
-                    cell: { row: rowIndex, col: colIndex },
+                    cell: {row: rowIndex, col: colIndex},
                     player: activePlayer
                 },
                 ...prevTurns
@@ -80,22 +81,22 @@ function App() {
             <div id="game-container">
                 <ol id="players" className="highlight-player">
                     <Player
-                        initialName="Player 1"
+                        initialName={PLAYERS.X}
                         symbol="X"
                         isActive={activePlayer === 'X'}
                         onChangeName={handlePlayerNameChange}
                     />
                     <Player
-                        initialName="Player 2"
+                        initialName={PLAYERS.O}
                         symbol="O"
                         isActive={activePlayer === 'O'}
                         onChangeName={handlePlayerNameChange}
                     />
                 </ol>
-                {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
-                <GameBoard onClickCell={handleClickCell} board={gameBoard} />
+                {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
+                <GameBoard onClickCell={handleClickCell} board={gameBoard}/>
             </div>
-            <Log turns={gameTurns} />
+            <Log turns={gameTurns}/>
         </main>
     );
 }
